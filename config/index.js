287a1bc2faa7c5c61b4df1a079ab28e5ac95b13e
jerome han <875212376@qq.com>
2017-05-24 18:20:36 +0800

    Update index.js

--- config/index.js
+++ config/index.js
@@ -10,8 +10,8 @@ var wechat_file = path.join(__dirname, '../file/wechat.txt');
 
 var config = {
     wechat: {
-        appID: 'wxd11a7c31ce251955',
-        appSecret: '2eaa18464930b5dc16a5cec6d320120c',
+        appID: 'wxd11a7c31ce251956',//你自己的appid
+        appSecret: '2eaa18464930b5dc16a5cec6d320120d',//你自己的appSecret
         token: 'wxmk',
         getAccessToken: function () {
             return util.readFileAsync(wechat_file);
